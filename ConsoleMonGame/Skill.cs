0639672a2b9c48566102f9fd4b8e65867b6a52ed
keith <25635@ma-web.nl>
2023-05-08 12:52:11 +0200

    add

---
+++ ConsoleMonGame/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMonGame
+{
+    internal class Skill
+    {
+        int damage;
+
+        int energyCost;
+
+        string name;
+    }
+}
